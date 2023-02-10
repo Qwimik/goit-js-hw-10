de6a279c4d016979e643929736e397078d047478
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-11 00:11:58 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
+import fetchCountries from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+// const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  listEl: document.querySelector('.country-list'),
+  divEl: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener('input', debounce(onInputChange, 300));
+
+function onInputChange() {
+  if (refs.inputEl.value === '' || refs.inputEl.value === ' ') {
+    return;
+  }
+  let value = refs.inputEl.value.trim();
+
+  fetchCountries(value)
+    .then(filterCountry)
+    .catch(err => {
+      console.log(err);
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function filterCountry(countries) {
+  if (countries.length >= 10) {
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  if (countries.length >= 2 && countries.length < 10) {
+    return renderCountries(countries);
+  }
+  if (countries.length > 0) {
+    renderCountry(countries);
+  }
+}
+
+function renderCountries(countries) {
+  refs.divEl.innerHTML = '';
+
+  countries.map(country => {
+    const countriesElems = `
+    <div class="countries">
+        <img src="${country.flags.svg}" alt="${country.name.official}" width="40"/>
+        <span>${country.name.official}</span>
+    </div>`;
+
+    refs.divEl.insertAdjacentHTML('beforeend', countriesElems);
+  });
+}
+
+function renderCountry(country) {
+  refs.divEl.innerHTML = '';
+  const lang = Object.values(country[0].languages);
+
+  const countryElems = `
+    <div class="country">
+      <div class="country-head">
+          <img src="${country[0].flags.svg}" alt="${
+    country[0].name.official
+  }" width="40" />
+          <span class="country-name">${country[0].name.official}</span>
+      </div>
+      <ul class="country-list">
+          <li class="country-item"><span class="country-item-span">Capital</span>: ${
+            country[0].capital
+          }</li>
+          <li class="country-item"><span class="country-item-span">Population</span>: ${
+            country[0].population
+          }</li>
+          <li class="country-item"><span class="country-item-span">Languages</span>: ${lang.join(
+            ', '
+          )}</li>
+      </ul>
+    </div>`;
+
+  refs.divEl.insertAdjacentHTML('beforeend', countryElems);
+}
