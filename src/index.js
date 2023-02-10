e40f6f7a16032acceff318ff02c6979d90651541
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-11 00:27:47 +0200

    add notify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import fetchCountries from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
